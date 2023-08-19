afbbc529b45ae0c4ded91a05ddb7e71ffc4adea0
pejoal <pejoal.official@gmail.com>
2023-08-19 22:06:20 +0200

    work on adding question

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -11,8 +11,26 @@ class QuestionController extends Controller {
   public function index() {
   }
 
-  public function store(QuestionRequest $request) {
+  public function store(Request $request) {
     dd($request->all());
+    // $data = $request->validate([
+    //   'questionText' => 'required|string',
+    //   'answers' => 'required|array|size:4',
+    //   'correctAnswerIndex' => 'required|integer|between:0,3',
+    // ]);
+
+    // $question = Question::create([
+    //   'text' => $data['questionText'],
+    //   'correct_answer' => $data['correctAnswerIndex'],
+    // ]);
+
+    // foreach ($data['answers'] as $index => $answer) {
+    //   Answer::create([
+    //     'question_id' => $question->id,
+    //     'text' => $answer,
+    //     'is_correct' => $index === $data['correctAnswerIndex'],
+    //   ]);
+    // }
   }
 
   public function update(Request $request, Question $question) {
--- resources/js/Components/Questions.vue
+++ resources/js/Components/Questions.vue
@@ -1,5 +1,7 @@
 <script setup>
 import { useForm } from "@inertiajs/vue3";
+import ResuableModal from "./ResuableModal.vue";
+import { ref } from "vue";
 const props = defineProps({
   type: {
     type: String,
@@ -20,8 +22,31 @@ const form = useForm({
   type: props.db_type,
 });
 
+const showModal = ref(false);
 const add_question = () => {
-  form.post(route("question.store"));
+  showModal.value = true;
+  // form.post(route("question.store"));
+};
+
+const questionText = ref('');
+const answers = ref(['', '', '', '']);
+const correctAnswerIndex = ref(null);
+
+const saveQuestion = () => {
+  if (correctAnswerIndex.value === null) {
+    alert('Please select the correct answer.');
+    return;
+  }
+
+  const question = {
+    questionText: questionText.value,
+    answers: answers.value,
+    correctAnswerIndex: correctAnswerIndex.value,
+  };
+
+  // Send the question to the Laravel backend using Inertia
+  // You can use `emit()` to send an event to the parent component for redirection.
+  // For example, `emit('question-saved', question);`
 };
 </script>
 <template>
@@ -29,6 +54,59 @@ const add_question = () => {
     <h3 class="text-center text-lg font-bold mt-2 mb-3">
       {{ trans(`words.${type}`) }}
     </h3>
+    <Teleport to="#modal">
+      <ResuableModal
+        :classes="['w-[90%] md:w-[85%] lg:w-[80%] h-[60%]']"
+        :header="trans('words.add_question')"
+        :show="showModal"
+        @close="showModal = false"
+      >
+        <template #content>
+          <div>
+            <h2>Create a Question</h2>
+            <form @submit.prevent="saveQuestion">
+              <div class="form-group">
+                <label for="question">Question:</label>
+                <input
+                  type="text"
+                  id="question"
+                  v-model="questionText"
+                  class="form-control"
+                  required
+                />
+              </div>
+              <div class="form-group">
+                <label>Answers:</label>
+                <div
+                  v-for="(answer, index) in answers"
+                  :key="index"
+                  class="form-check"
+                >
+                  <input
+                    type="radio"
+                    :id="'answer' + index"
+                    :value="index"
+                    v-model="correctAnswerIndex"
+                    class="form-check-input"
+                  />
+                  <label :for="'answer' + index" class="form-check-label">
+                    <input
+                      type="text"
+                      v-model="answers[index]"
+                      class="form-control"
+                      required
+                    />
+                  </label>
+                </div>
+              </div>
+              <button type="submit" class="btn btn-primary">
+                Save Question
+              </button>
+            </form>
+          </div>
+        </template>
+      </ResuableModal>
+    </Teleport>
     <section class="flex items-center justify-between">
       <p>{{ questions_count }} {{ trans("words.questions") }}</p>
       <section class="flex gap-2">
