be90583a9dd419e17768899ad26260e3e7a5acb6
pejoal <pejoal.official@gmail.com>
2023-08-22 21:30:05 +0200

    Quiz Screen 3

--- resources/js/Pages/Quiz/Start.vue
+++ resources/js/Pages/Quiz/Start.vue
@@ -12,25 +12,22 @@ const currentIndex = ref(0);
 const userAnswers = ref([]);
 
 const moveToNextQuestion = (selectedAnswerId) => {
+
   userAnswers.value.push({
     question_id: props.questions[currentIndex.value].id,
     answer_id: selectedAnswerId,
   });
-  if (currentIndex.value < props.questions.length - 1) {
-    currentIndex.value++;
-  } else {
-    // All questions answered, send userAnswers to the server
+  currentIndex.value++;
+  if (currentIndex.value > props.questions.length - 1) {
     submitAnswers(userAnswers.value);
   }
 };
 
 const submitAnswers = (answers) => {
-  // Here, you can make an API request to send userAnswers to the server
   console.log(answers);
 };
 </script>
 <template>
-  <!-- ... (previous code) -->
   <section class="p-4">
     <div v-if="currentIndex < questions.length">
       <h3 class="text-lg font-semibold">Question {{ currentIndex + 1 }}</h3>
