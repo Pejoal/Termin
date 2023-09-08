e2c74cf80bc275aeb5df8e0d93d947764a4913bf
pejoal <pejoal.official@gmail.com>
2023-09-08 21:15:40 +0200

    Add Client2

--- database/seeders/SetupSeeder.php
+++ database/seeders/SetupSeeder.php
@@ -37,6 +37,17 @@ class SetupSeeder extends Seeder {
       'type' => 'client',
     ]);
 
+    User::create([
+      'firstname' => 'client',
+      'lastname' => '2',
+      'username' => 'client2',
+      'gender' => 'female',
+      'email' => 'client2@example.com',
+      'email_verified_at' => now(),
+      'password' => bcrypt('11111111'),
+      'type' => 'client',
+    ]);
+
   
   }
 }
